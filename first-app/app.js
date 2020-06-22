83f2d77ad6f5a20a35fb88d71139450fd721af0d
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-06-22 13:04:35 -0400

    added practcice

--- first-app/app.js
+++ first-app/app.js
@@ -11,4 +11,56 @@ const path = require('path');
 
 var pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+// OS module.
+const os = require('os');
+const totalmemory = os.totalmem();
+const freeMemory = os.freemem();
+
+// console.log("total Memory:" + totalmemory);
+
+// Template string
+// ES6 
+// console.log(`Total memory: ${totalmemory}`);
+// console.log(`free memory: ${freeMemory}`);
+
+// file system 
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+// readirr is a async function. has a call bacl function
+
+// const files1 = fs.readdir('./', function(err, files){
+//    if (err) console.log('Error', err); 
+//    else console.log('Result', files);
+// });
+// console.log(files1)
+
+// events////////////////////////
+// class Upper case letetrs EventEmitter.
+const EvenEmitter = require('events');
+// creating new instance using new Key word.
+const emitter = new EvenEmitter();
+
+//register a listener
+// has a call back function.
+emitter.on("messagelogged",(e)=>{
+console.log("listener called", e);
+} ) 
+
+// raise an event
+//making a noise passing an event. from emitter.on.
+emitter.emit("messagelogged",{
+    id : 1, 
+    url:'http://'
+});
+
+// raise:logging event (data:message)
+
+
+
+
+
