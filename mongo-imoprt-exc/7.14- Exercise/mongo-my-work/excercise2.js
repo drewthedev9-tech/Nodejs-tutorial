d281673dfb046e9658edfcea301931cf1df7f8e1
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-10-26 17:55:26 -0400

    addingmongo-connection excercise

---
+++ mongo-imoprt-exc/7.14- Exercise/mongo-my-work/excercise2.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+
+    .then(()=> console.log('connected to mongDB ...'))
+    .catch(err=> console.error("could not connect to mongodb..", err));
+
+
+ const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+// see exc2 image for criteria taht this fucntion meets.
+async function getCoursesExc2() {
+    // in meaning has 'backend or "frontend" in the query
+    const courses = await Course
+  .find({ isPublished: true, tags: {$in:['frontend','backend']}})
+//   desc order
+  .sort({price:-1})
+  .select(' name author price' );
+
+  console.log(courses);
+}
+
+getCoursesExc2();
---
+++ mongo-imoprt-exc/7.14- Exercise/mongo-my-work/excercise3.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+
+    .then(()=> console.log('connected to mongDB ...'))
+    .catch(err=> console.error("could not connect to mongodb..", err));
+
+
+ const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+// see exc3 image for criteria taht this fucntion meets.
+async function getCoursesExc3() {
+    // in meaning has 'backend or "frontend" in the query
+    const courses = await Course
+  .find({ isPublished: true})
+//   $gte is greater than or equal to "mongoDB operators".
+  .or([
+      {price:{$gte: 15}}
+  ])
+//   desc order
+  .sort({price:-1})
+  // get name,author, price
+  .select(' name author price' );
+
+  console.log(courses);
+}
+
+getCoursesExc3();
